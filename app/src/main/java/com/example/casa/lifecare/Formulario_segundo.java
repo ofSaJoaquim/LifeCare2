7cc626f694f4f6cd6bca300f42a8661ddc6da539
Luiz joaquim <luiz.sa.joaquim@gmail,com>
2018-08-25 23:29:47 -0300

    Adicionado as imagens com fundo transparente 25/08/2018 Corrigido os Assets das imagens.25/08/2018

---
+++ app/src/main/java/com/example/casa/lifecare/Formulario_segundo.java
@@ -0,0 +1,13 @@
+package com.example.casa.lifecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Formulario_segundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_segundo);
+    }
+}
