24d18942d92addc4a45c96087b0b26dec737f5d2
Luiz joaquim <luiz.sa.joaquim@gmail,com>
2018-08-26 21:41:49 -0300

    Adicionado as imagens com fundo transparente 25/08/2018                           Corrigido os Assets das imagens.25/08/2018                           Terminado segundo form                           Comando para trocar de tela                           iniciado dimmer 26/08/2018
    Adicionando tela de recados 26/08/2018

--- app/src/main/java/com/example/casa/lifecare/CadastroRemedio.java
+++ app/src/main/java/com/example/casa/lifecare/CadastroRemedio.java
@@ -1,7 +1,13 @@
 package com.example.casa.lifecare;
 
+import android.app.NotificationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroRemedio extends AppCompatActivity {
 
@@ -9,6 +15,39 @@ public class CadastroRemedio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_remedio);
+        Uri som = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        final NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.lifecare)
+                        .setContentTitle("Hora de tomar rémedio")
+                        .setContentText("Gardenau")
+                        .setAutoCancel(true)
+                       .setSound(som)
+                        .setVibrate(new long[]{ 100, 250, 100, 500 })
+                        .setLights(100, 500, 100);;
 
+
+        final int mNotificationId = 001;
+
+        final NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        final Button proximo = (Button) findViewById(R.id.button4);
+
+
+        proximo.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public  void  onClick(View v){
+               /* Toast.makeText(Formulario_Primeiro.this, "Hello World",
+                        Toast.LENGTH_LONG).show();*/
+                mostrar(mNotifyMgr,mNotificationId,mBuilder);
+
+            }
+        } );
+
+
+    }
+    public void mostrar(NotificationManager n, int id,  NotificationCompat.Builder builder){
+        n.notify(id, builder.build());
     }
 }
--- app/src/main/java/com/example/casa/lifecare/TesteRecadosGrupo.java
+++ app/src/main/java/com/example/casa/lifecare/TesteRecadosGrupo.java
@@ -1,6 +1,8 @@
 package com.example.casa.lifecare;
 
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -28,6 +30,7 @@ public class TesteRecadosGrupo extends AppCompatActivity {
 
             }
         } );
+
     }
     //infla o menu no toolbar teste
     public boolean onCreateOptionsMenu(Menu menu) {
