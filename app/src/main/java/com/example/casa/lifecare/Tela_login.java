da81325bc49bd4693a57dfb37e24981605e0e3ba
Junior Mendes <vilmarmendesjunior@hotmail.com>
2018-08-27 02:07:39 -0300

    Adicionada ação no botão LOGIN

--- app/src/main/java/com/example/casa/lifecare/Tela_login.java
+++ app/src/main/java/com/example/casa/lifecare/Tela_login.java
@@ -1,7 +1,10 @@
 package com.example.casa.lifecare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Tela_login extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class Tela_login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void proximaTela(View view){
+
+        Intent intent = new Intent(this, Formulario_Primeiro.class);
+        startActivity(intent);
+    }
+
+
 }
