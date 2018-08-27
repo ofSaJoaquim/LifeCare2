eda3ce7252e62e83eaee204572c4fbfc602a92da
Luiz joaquim <luiz.sa.joaquim@gmail,com>
2018-08-26 21:07:44 -0300

    Adicionado as imagens com fundo transparente 25/08/2018                           Corrigido os Assets das imagens.25/08/2018                           Terminado segundo form                           Comando para trocar de tela                           iniciado dimmer 26/08/2018
    Adicionando tela de recados 26/08/2018

--- app/src/main/java/com/example/casa/lifecare/TesteRecadosGrupo.java
+++ app/src/main/java/com/example/casa/lifecare/TesteRecadosGrupo.java
@@ -1,10 +1,13 @@
 package com.example.casa.lifecare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class TesteRecadosGrupo extends AppCompatActivity {
 
@@ -15,6 +18,16 @@ public class TesteRecadosGrupo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar2); // get the reference of Toolbar
         setSupportActionBar(toolbar); // Setting/replace toolbar as the ActionBar
         toolbar.setTitle("");
+        final Button proximo = (Button) findViewById(R.id.button3);
+        proximo.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public  void  onClick(View v){
+               /* Toast.makeText(Formulario_Primeiro.this, "Hello World",
+                        Toast.LENGTH_LONG).show();*/
+                proximoFormulario();
+
+            }
+        } );
     }
     //infla o menu no toolbar
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,5 +35,9 @@ public class TesteRecadosGrupo extends AppCompatActivity {
         inflater.inflate(R.menu.menu2, menu);
         return true;
     }
+    public void proximoFormulario(){
+        Intent intent = new Intent(this, CadastroRemedio.class);
+        startActivity(intent);
 
+    }
 }
