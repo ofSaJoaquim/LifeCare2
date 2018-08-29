e86b581b8b2e2124acd9c18a89a6ea81bc12997b
Junior Mendes <vilmarmendesjunior@hotmail.com>
2018-08-29 00:15:44 -0300

    Tela de cadastro e imagem de fundo inseridas

---
+++ app/src/main/java/com/example/casa/lifecare/CadastroUsuario.java
@@ -0,0 +1,14 @@
+package com.example.casa.lifecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class CadastroUsuario extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
