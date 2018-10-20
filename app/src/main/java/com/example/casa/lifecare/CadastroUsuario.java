3d6bce86b1608360845c9d47932227a6580a5317
Luiz joaquim <luiz.sa.joaquim@gmail,com>
2018-10-20 15:42:09 -0300

    Adicionado os validadores para o cadastro de usuário. Removidos campos e atributos não usados.

--- app/src/main/java/com/example/casa/lifecare/CadastroUsuario.java
+++ app/src/main/java/com/example/casa/lifecare/CadastroUsuario.java
@@ -4,28 +4,21 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.Toast;
 
-import com.example.casa.lifecare.Servicos.CarregarEntidade;
 import com.example.casa.lifecare.Servicos.PostarEntidade;
 import com.example.casa.lifecare.entidades.Cidade;
 import com.example.casa.lifecare.entidades.Paciente;
-import com.example.casa.lifecare.entidades.Usuario;
-import com.example.casa.lifecare.utils.SimulaDB;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 
 public class CadastroUsuario extends AppCompatActivity {
@@ -35,96 +28,128 @@ public class CadastroUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
-        final EditText nomeCampo = (EditText) findViewById(R.id.txtCadNome);
-
-        final EditText senhaCampo = (EditText) findViewById(R.id.txt_senha);
-        final EditText emailCampo = (EditText) findViewById(R.id.txtCadEmail);
-        final EditText cpfCampo = (EditText) findViewById(R.id.txtCadCPF);
-        final EditText telefoneCampo = (EditText) findViewById(R.id.txtCadTelefone);
-
-
-        final RadioButton masc = (RadioButton)findViewById(R.id.rdMasc);
-
-
-        final RadioButton solteiro = (RadioButton)findViewById(R.id.rdSolteiro);
-        final RadioButton casado = (RadioButton)findViewById(R.id.rdCasado);
-        final RadioButton divorciado = (RadioButton)findViewById(R.id.rdDivorciado);
-        final RadioButton separado = (RadioButton)findViewById(R.id.rdSeparado);
-        final RadioButton viuvo = (RadioButton)findViewById(R.id.rdViuvo);
-
-        final EditText dataNascimentoCampo = (EditText) findViewById(R.id.txtCadNasc);
+        paciente = new Paciente();
+        final EditText nome = findViewById(R.id.txtCadNome);
+        final EditText senha = findViewById(R.id.txtCadSenha);
+        final EditText email = findViewById(R.id.txtCadEmail);
+        final RadioButton masc = findViewById(R.id.rdMasc);
+        final EditText idade =  findViewById(R.id.txtCadIdade);
 
 
         final Button proximo = (Button) findViewById(R.id.cadUsuarioConfirma);
         proximo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               /* Toast.makeText(Formulario_Primeiro.this, "Hello World",
-                        Toast.LENGTH_LONG).show();*/
-                //tem que criar uma validação antes de inserir
-                String nome = nomeCampo.getText().toString();
-                String usuarioLogin = emailCampo.getText().toString();
-                String senha = nomeCampo.getText().toString();
-                String email = emailCampo.getText().toString();
-               String cpf = cpfCampo.getText().toString();
-               Long telefone = Long.parseLong(telefoneCampo.getText().toString());
+                for(int count=0;count<1;count++) {
+                    if (nome.getText().toString().length() < 1) {
+                        nome.setError("Campo obrigatório");
+                        nome.setFocusable(true);
+                        nome.requestFocus();
+                        break;
+                    } else if (!validarNome(nome.getText().toString())) {
+                        nome.setError("Nome inválido");
+                        nome.setFocusable(true);
+                        nome.requestFocus();
+                        break;
+                    } else {
+                        paciente.setNome(nome.getText().toString());
 
-                Boolean sexo=false;
-                if(masc.isSelected())sexo=true;
-                Integer estadoCivil = 0;
-                if(casado.isSelected())estadoCivil=1;
-                else if(divorciado.isSelected())estadoCivil=2;
-                else if(separado.isSelected())estadoCivil=3;
-                else if(viuvo.isSelected())estadoCivil=4;
+                    }
 
-                Integer dataNascimento[] = converteData(dataNascimentoCampo);
-                Integer diaNascimento = dataNascimento[0];
-                Integer mesNascimento = dataNascimento[1];
-                Integer anoNascimento = dataNascimento[2];
-                // SimulaDB.usuario = new Usuario(nome,usuarioLogin,senha,email,cpf,telefone,sexo,estadoCivil,diaNascimento,mesNascimento,anoNascimento);
-                Cidade cidade=null;
-                cadastrar(nome,2018-dataNascimento[2],email,senha,cidade);
+                    //Válida a idade
+                    if (idade.getText().toString().length() < 1) {
+                        idade.setError("Campo obrigatório");
+                        idade.setFocusable(true);
+                        idade.requestFocus();
+                        break;
+                    } else if (!(validarIdade(idade.getText().toString()))) {
+                        idade.setError("Idade inválida");
+                        idade.setFocusable(true);
+                        idade.requestFocus();
+                        break;
+                    } else {
+                        try {
+                            Integer idadec = Integer.parseInt(idade.getText().toString());
+                            if (idadec < -1) {
+                                idade.setError("Idade não pode ser negativo");
+                                idade.setFocusable(true);
+                                idade.requestFocus();
+                                break;
+                            } else if (idadec > 150) {
+                                idade.setError("Você tem 150 anos, ligue para a central");
+                                idade.setFocusable(true);
+                                idade.requestFocus();
+                                break;
+                            } else paciente.setIdade(idadec);
 
-                //string xml adicionar
-               /* Toast.makeText(CadastroUsuario.this, "Congratulations",
-                        Toast.LENGTH_LONG).show();*/
-                //proximoForm();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+
+                        }
+
+                    }
+
+                    if (email.getText().toString().length() < 1) {
+                        email.setError("Campo obrigatório");
+                        email.setFocusable(true);
+                        email.requestFocus();
+                        break;
+                    }
+                    else if(!validarEmail(email.getText().toString())){
+                        email.setError("Email inválido");
+                        email.setFocusable(true);
+                        email.requestFocus();
+                        break;
+                    }
+                    else{
+                        paciente.setEmail(email.getText().toString());
+                    }
+                    if(senha.getText().toString().length()<4){
+                        email.setError("Senha no mínimo 4 caracteres");
+                        email.setFocusable(true);
+                        email.requestFocus();
+                        break;
+                    }
+                    else {
+                        paciente.setEmail(senha.getText().toString());
+                    }
+                    cadastrar();
+
+
+
+                }
 
             }
         });
     }
+private boolean validarNome(String nome ){
+        String expressao="[a-zA-Z[-]]+[ ][a-zA-Z0-9[-][ ]]+";
+        return nome.matches(expressao);
+
+}
+
+private boolean validarIdade(String idade){
+    String expressao="[0-9]+";
+    return idade.matches(expressao);
+}
+
+private boolean validarEmail(String email){
+        String expressao="[a-zA-z0-9[-_.]]+@[a-zA-z0-9[-_.]]+[[a-zA-z]]";
+        return email.matches(expressao);
+}
 
 
-    //metodo para testar entrada da data de nascimento, melhorar usando outra classe propria para data e consertar a tradução
-    private Integer[] converteData(EditText dataorigem) {
-        String dataTexto = dataorigem.getText().toString();
-        Integer data[] = new Integer[3];
-        if (dataTexto.matches("\\d\\d/\\d\\d/\\d\\d\\d\\d")) {
-            String dataSepara[] = dataTexto.split("/");
 
-            data[0] = Integer.parseInt(dataSepara[0]);
-            data[1] = Integer.parseInt(dataSepara[1]);
-            data[2] = Integer.parseInt(dataSepara[2]);
-         /*   Toast.makeText(CadastroUsuario.this, data[0] + "/" + data[1] + "/" + data[2],
-                    Toast.LENGTH_LONG).show();*/
-
-            return data;
-        } else {
-
-            Integer teste[]={22,12,2012};
-            return teste;
-        }
-
-    }
     private void proximoForm(){
         Intent intent = new Intent(this, Formulario_Primeiro.class);
         startActivity(intent);
     }
-    private void cadastrar(String nome, Integer idade, String email, String senha, Cidade cidade){
-        cidade = new Cidade(1,"Tubarão","Santa Catarina");
+    private void cadastrar(){
 
-        paciente = new Paciente(nome,idade,email,senha,cidade);
+
+
         PostaPaciente pp = new PostaPaciente();
+       pp.execute();
 
     }
 
